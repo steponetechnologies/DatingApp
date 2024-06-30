409ec78085d355eb0057ff47dbe61a8a08f1e361
pranay kumar <pranay.kumar9888@gmail.com>
2024-06-30 12:15:30 +0530

    End of Section 2

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api;
+
+
+public class DataContext:DbContext
+{
+    public DataContext(DbContextOptions options):base(options)
+    {
+    }
+    
+     public DbSet<AppUser> Users {get;set;}
+}
